ffda0e68a23b84edec6837e735a83b65a876dca1
Wingorpoi <90994123+Wingorpoi@users.noreply.github.com>
2023-03-27 14:36:28 +0800

    a

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-
+import Header from "./Header"
+import Nav from "./Nav"
+import Main from "./Main"
+import Footer from "./Footer"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hi
-      Hi
-    </div>
+  <>
+  <Header/>
+  <Nav/>
+  <Main/>
+  <Footer/>
+  
+  </>
   );
 }
 
---
+++ src/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+        <img></img>
+        <ul>
+            <li><a>a</a></li>
+            <li><a>b</a></li>
+            <li><a>c</a></li>
+            <li><a>d</a></li>
+            <li><a>e</a></li>
+            <li><a>f</a></li>
+        </ul>
+    </div>
+  )
+}
+
+export default Nav
