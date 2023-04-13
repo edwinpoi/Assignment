3bb24297d4cf9ed9416d7f9aaeca86674731e616
Wingorpoi <90994123+Wingorpoi@users.noreply.github.com>
2023-04-13 17:41:13 +0800

    m

--- src/bookingpage/bookingForm.js
+++ src/bookingpage/bookingForm.js
@@ -1,28 +1,10 @@
-import { useEffect, useState, useReducer } from "react";
+import { useReducer } from "react";
 import { fetchAPI, submitAPI } from "../bookingApi";
 import { useNavigate } from "react-router-dom";
 import { useFormik } from "formik";
 import * as Yup from "yup";
-const Main = (props) => {
-  const formik = useFormik({
-    initialValues: {
-      availableTimes: "",
-      date: "",
-      time: "",
-      guests: "",
-      occasion: "",
-    },validationSchema:Yup.object({
-    availableTimes:Yup.date().required('Date is not empty'),
-    date:Yup.string().required("Time is not empty"),
-    guests:Yup.number().min(1).max(10).required("Guest is not empty"),
-    occasion:Yup.string().required("Occasion is not empty"),
-    }),
-    onSubmit: (values) => {
-      console.log(values);
 
-    },
-  });
-
+const Main = () => {
   const updateTimes = (state, action) => {
     return fetchAPI(state);
   };
@@ -31,26 +13,39 @@ const Main = (props) => {
     updateTimes,
     fetchAPI(initializeTimes)
   );
-  const [date, setDate] = useState(formatDate());
-  const [time, setTime] = useState("");
-  const [guests, setGuests] = useState(1);
-  const [occasion, setOccasion] = useState("Birthday");
   const navigate = useNavigate();
 
-  const submitForm = (event) => {
-    event.preventDefault();
-    const data = {
-      date: date,
-      time: time,
-      guests: guests,
-      occasion: occasion,
-    };
+  const formik = useFormik({
+    initialValues: {
+      name:" ",
+      phoneNumber:" ",
+      email:" ",
+      date: formatDate(),
+      time: availableTimes[0],
+      guests: 1,
+      occasion: "Birthday",
+    },
 
-    if (submitAPI(data) == true) {
-      console.log("ok");
-      navigate("/confirmPage");
-    } else console.log("bad");
-  };
+    validationSchema: Yup.object({
+      date: Yup.string().required("Date is not empty"),
+      time: Yup.string().required("Time is not empty"),
+      guests: Yup.number()
+        .min(1, "Guest must be at least 1")
+        .max(25)
+        .required("Guest is not empty"),
+      occasion: Yup.string().required("Occasion is not empty"),
+      name:Yup.string().min(3,"Character at least 3 ").required("Name is not empty"),
+      phoneNumber:Yup.string().min(8).required("number is not empty"),
+      email:Yup.string().min(6).required("email is not empty"),
+    }),
+
+    onSubmit: (values) => {
+      if (submitAPI(values) == true) {
+        console.log(values);
+        navigate("/confirmPage", { state: values });
+      } else console.log("bad");
+    },
+  });
 
   function formatDate(date = new Date()) {
     var day, month, year;
@@ -69,6 +64,7 @@ const Main = (props) => {
   const changeTimeFormat = (data) => {
     const test = data.slice(8, 10);
     dispatch(test);
+    formik.values.date = data;
   };
 
   const availTimeList = availableTimes.map((number, index) => {
@@ -79,74 +75,126 @@ const Main = (props) => {
     );
   });
 
-  useEffect(() => {
-    console.log("Date:", { date });
-  }, [date]);
-  useEffect(() => {
-    console.log("Time:", { time });
-  }, [time]);
-  useEffect(() => {
-    console.log("Guests:", { guests });
-  }, [guests]);
-  useEffect(() => {
-    console.log("Occasion:", { occasion });
-  }, [occasion]);
-  useEffect(() => {
-    console.log("available Time :", { availableTimes });
-  }, [availableTimes]);
-
   return (
-    <div>
+    <div className="formContainer">
       <form onSubmit={formik.handleSubmit}>
-        <label for="res-date">Choose date</label>
-        <input
-          type="date"
-          id="date"
-          name="date"
-          onChange={(e) => {
-            setDate(e.target.value);
-            changeTimeFormat(e.target.value);
-          }}
-          value={date}
-        />
+        <h1>Table booking</h1>
 
-        <label for="res-time">Choose time</label>
-        <select
-          id="time"
-          name="time"
-          onChange={(e) => {
-            setTime(e.target.value);
-          }}
-          value={time}
-        >
-          {availTimeList}
-        </select>
-        <label for="guests">Number of guests</label>
-        <input
-          type="number"
-          placeholder="1"
-          min="1"
-          max="10"
-          id="guests"
-          name="guests"
-          onChange={(e) => {
-            setGuests(e.target.value);
-          }}
-          value={guests}
-        />
-        <label for="occasion">Occasion</label>
-        <select
-          id="occasion"
-          name="occasion"
-          onChange={(e) => {
-            setOccasion(e.target.value);
-          }}
-          value={occasion}
-        >
-          <option value="Birthday">Birthday</option>
-          <option value="Anniversary">Anniversary</option>
-        </select>
-        <input type="submit" value="Make Your reservation" />
+
+        <div>
+          <label for="name">Name:</label>
+          <br />
+          <input
+            type="text"
+            placeholder=""
+            id="name"
+            name="name"
+            aria-label="Your name"
+            {...formik.getFieldProps("name")}
+          />
+
+          {formik.touched.name && formik.errors.name ? (
+            <div className="formError">{formik.errors.name}</div>
+          ) : null}
+        </div>
+
+
+        <div>
+          <label for="phoneNumber">Phone number:</label>
+          <br />
+          <input
+            type="tel"
+            placeholder=""
+            id="phoneNumber"
+            name="phoneNumber"
+            aria-label="Your phone number"
+            {...formik.getFieldProps("phoneNumber")}
+          />
+
+          {formik.touched.phoneNumber && formik.errors.phoneNumber ? (
+            <div className="formError">{formik.errors.phoneNumber}</div>
+          ) : null}
+        </div>
+
+        <div>
+          <label for="email">Email:</label>
+          <br />
+          <input
+            type="email"
+            placeholder=""
+            id="email"
+            name="email"
+            aria-label="Your email"
+            {...formik.getFieldProps("email")}
+          />
+
+          {formik.touched.email && formik.errors.email ? (
+            <div className="formError">{formik.errors.email}</div>
+          ) : null}
+        </div>
+
+        <div>
+          <label for="res-date">Date:</label>
+          <br />
+          <input
+            type="date"
+            id="date"
+            name="date"
+            aria-label="Booking Date"
+            onChange={(e) => {
+              changeTimeFormat(e.target.value);
+            }}
+            onBlur={formik.handleBlur}
+            value={formik.values.date}
+          />
+            {formik.touched.date && formik.errors.date ? (
+            <div className="formError">{formik.errors.date}</div>
+          ) : null}
+        </div>
+        <div>
+          <label for="res-time">Time</label>
+          <br />
+          <select id="time" name="time" aria-label="Booking Time" {...formik.getFieldProps("time")}>
+            {availTimeList}
+          </select>
+
+          {formik.touched.time && formik.errors.time ? (
+            <div className="formError">{formik.errors.time}</div>
+          ) : null}
+        </div>
+        <div>
+          <label for="guests">Number of Guests:</label>
+          <br />
+          <input
+            type="number"
+            placeholder=""
+            id="guests"
+            name="guests"
+            aria-label="Number of guest"
+            {...formik.getFieldProps("guests")}
+          />
+
+          {formik.touched.guests && formik.errors.guests ? (
+            <div className="formError">{formik.errors.guests}</div>
+          ) : null}
+        </div>
+        <div>
+          <label for="occasion">Occasion:</label><br/>
+          <select
+            id="occasion"
+            name="occasion"
+            aria-label="Occasion"
+            {...formik.getFieldProps("occasion")}
+          >
+            <option value="Birthday">Birthday</option>
+            <option value="Anniversary">Anniversary</option>
+          </select>
+          {formik.touched.occasion && formik.errors.occasion ? (
+            <div className="formError">{formik.errors.occasion}</div>
+          ) : null}
+        </div><br/>
+
+        <input type="submit" value="Make Your reservation"  aria-label="submit"/>
       </form>
     </div>
   );
--- src/bookingpage/confirmedBooking.js
+++ src/bookingpage/confirmedBooking.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { useLocation } from "react-router-dom";
 
-const confirmedBooking = () => {
+const ConfirmedBooking = (props) => {
+
+  const location = useLocation();
+  const data =location.state;
   return (
-    <div>
-        <h1>Booking Confirm:</h1>
-
+    <div className='formContainer'>
+        <h1>Booking Confirmed:</h1>
+        <div>Name:{data.name}</div>
+        <div>email:{data.email}</div>
+        <div>phoneNumber:{data.phoneNumber}</div>
+        <div>Date:{data.date}</div>
+        <div>Time:{data.time}</div>
+        <div>Guests:{data.guests}</div>
+        <div>Ocasion:{data.occasion}</div>
     </div>
   )
 }
 
-export default confirmedBooking
+export default ConfirmedBooking
