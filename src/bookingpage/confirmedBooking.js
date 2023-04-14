938bf0d2e61aad3a7763d5e5d06ca032117b1e77
Wingorpoi <90994123+Wingorpoi@users.noreply.github.com>
2023-04-14 19:39:53 +0800

    a

--- src/bookingpage/confirmedBooking.js
+++ src/bookingpage/confirmedBooking.js
@@ -6,15 +6,18 @@ const ConfirmedBooking = (props) => {
   const location = useLocation();
   const data =location.state;
   return (
-    <div className='formContainer'>
+    <div className='confirmContainer'>
         <h1>Booking Confirmed:</h1>
+        <h3>Thank for your booking, hope you love our serve</h3>
+        <div>
         <div>Name:{data.name}</div>
-        <div>email:{data.email}</div>
-        <div>phoneNumber:{data.phoneNumber}</div>
+        <div>Email:{data.email}</div>
+        <div>Phone Number:{data.phoneNumber}</div>
         <div>Date:{data.date}</div>
         <div>Time:{data.time}</div>
         <div>Guests:{data.guests}</div>
         <div>Ocasion:{data.occasion}</div>
+        </div>
     </div>
   )
 }
